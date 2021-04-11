d5d0d14d45e319c38c40282f2ab7c0785a79f24e
Christopher Breen <C.jbreen61@gmail.com>
2021-04-11 16:23:04 -0400

    added custom serializer to render json for  shows

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,24 @@
 class Character < ApplicationRecord
   belongs_to :show
+
+
+  def self.arr_to_json
+    self.all.map do |s|
+        s.instance_to_json
+    end
+
+
+def instance_to_json
+    {   id: self.id,
+        name: self.title,
+        status: self.status,
+        release_date: self.release_date,
+        seasons: self.seasons,
+        likes: self.likes,
+        dislikes: self.dislikes,
+        description: self.description,
+        characters: self.characters.arrc_to_json
+    }
+end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,24 @@
 class Show < ApplicationRecord
+    has_many :characters
+
+
+    def self.arr_to_json
+        self.all.map do |s|
+            s.instance_to_json
+        end
+    end
+
+    def instance_to_json
+        {   id: self.id,
+            name: self.title,
+            status: self.status,
+            release_date: self.release_date,
+            seasons: self.seasons,
+            likes: self.likes,
+            dislikes: self.dislikes,
+            description: self.description,
+            characters: self.characters.arrc_to_json
+        }
+    end
+
 end
